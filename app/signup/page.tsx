85c775ef0f1f077780fcf3770255533fe9eb93f1
srikanthnani <srikanthnani1202@gmail.com>
2023-10-29 22:50:27 +0530

    signup :sparkles:

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import React from "react";
-import { Flex, TextField, Button, Text } from "@radix-ui/themes";
+import { Flex, TextField, Button, Text, Callout } from "@radix-ui/themes";
 import AuthLayout from "@/components/AuthLayout";
 import Link from "next/link";
+import useSignUp from "@/hooks/useSignUp";
+import useInputOnChange from "@/lib/useInputOnChange";
 
 const SignUp = () => {
+  const { inputs, inputErrors, loading, onSignUp, setInputs, setInputErrors } =
+    useSignUp();
   return (
     <AuthLayout>
       <Flex direction={"column"} gap={"3"} width={"100%"}>
@@ -16,10 +20,43 @@ const SignUp = () => {
             Sign up to explore a world of possibilities.
           </Text>
         </Flex>
-        <TextField.Input size={"3"} placeholder={"Enter name"} />
-        <TextField.Input size={"3"} placeholder={"Enter email"} />
-        <TextField.Input size={"3"} placeholder={"Enter password"} />
-        <Button size={"3"}>Get Started</Button>
+        {/* INPUT:NAME */}
+        <TextField.Input
+          name={"name"}
+          value={inputs?.name}
+          size={"3"}
+          placeholder={"Enter name"}
+          onChange={(e) => useInputOnChange({ e, setInputs, setInputErrors })()}
+        />
+        {/* INPUT:EMAIL */}
+        <TextField.Input
+          name={"email"}
+          value={inputs?.email}
+          size={"3"}
+          placeholder={"Enter email"}
+          onChange={(e) => useInputOnChange({ e, setInputs, setInputErrors })()}
+        />
+        {/* INPUT:PASSWORD */}
+        <TextField.Input
+          name={"password"}
+          value={inputs?.password}
+          size={"3"}
+          placeholder={"Enter password"}
+          onChange={(e) => useInputOnChange({ e, setInputs, setInputErrors })()}
+        />
+        {/* alert */}
+        {(inputErrors?.name || inputErrors?.email || inputErrors?.password) && (
+          <Callout.Root size="1" color="red">
+            <Callout.Text>
+              {inputErrors?.email || inputErrors?.password}
+            </Callout.Text>
+          </Callout.Root>
+        )}
+        {/* SUBMIT BUTTON */}
+        <Button size={"3"} onClick={onSignUp} disabled={loading}>
+          {loading ? "Loading..." : "Continue"}
+        </Button>
+        {/* SIGN-IN */}
         <Text className="self-center pt-4" weight={"light"}>
           already have an account?{" "}
           <Link
---
+++ hooks/useSignUp.ts
@@ -0,0 +1,84 @@
+import { NewUser } from "@/types/types.auth";
+import { useState } from "react";
+import { createUserAccount } from "@/appwrite/auth";
+import { emailRegex } from "@/lib/reg";
+
+const useSignUp = () => {
+  const [inputs, setInputs] = useState<NewUser>({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [inputErrors, setInputErrors] = useState<NewUser>({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const onSignUp = async () => {
+    // Clear any previous input errors
+    setInputErrors({ email: "", password: "", name: "" });
+
+    // Destructure email and password from inputs
+    const { email, password, name } = inputs;
+
+    // Input validations
+    if (!name) {
+      setInputErrors((prevErrors) => ({
+        ...prevErrors,
+        name: "Name is required",
+      }));
+      return;
+    }
+    if (!email) {
+      setInputErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Email is required",
+      }));
+      return;
+    }
+    if (!emailRegex.test(email)) {
+      setInputErrors((prevErrors) => ({
+        ...prevErrors,
+        email: "Invalid email address",
+      }));
+      return;
+    }
+    if (!password) {
+      setInputErrors((prevErrors) => ({
+        ...prevErrors,
+        password: "Password is required",
+      }));
+      return;
+    }
+
+    setLoading(true);
+
+    // appwrite create function
+    const result = await createUserAccount({ name, email, password });
+    //@ts-ignore
+    if (result?.accountId) {
+      console.log("user signed up");
+    } else {
+      setInputErrors((prevErrors) => ({
+        ...prevErrors,
+        //@ts-ignore
+        email: result?.message || "something went wrong",
+      }));
+    }
+
+    setLoading(false);
+  };
+
+  return {
+    inputs,
+    inputErrors,
+    loading,
+    onSignUp,
+    setInputs,
+    setInputErrors,
+  };
+};
+
+export default useSignUp;
